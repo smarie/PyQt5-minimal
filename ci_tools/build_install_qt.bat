ea18276666a5d1d731ca17b3ebc900034f52192b
Sylvain Marie <sylvain.marie@schneider-electric.com>
2017-10-10 09:55:33 +0200

    First attempt of appveyor build for windows. Setting up msys2 first

--- ci_tools/build_install_qt.bat
+++ ci_tools/build_install_qt.bat
@@ -44,17 +44,28 @@ echo QT_PCRE_SRC: %QT_PCRE_SRC%
 IF %COMPILER%==msys2 (
 
     SET "PATH=%MSYS2_DIR%\%MSYSTEM%\bin;%MSYS2_DIR%\usr\bin;%PATH%"
-    bash -lc "pacman -S --needed --noconfirm pacman-mirrors"
-    bash -lc "pacman -S --needed --noconfirm git"
+    REM bash -lc "pacman -S --needed --noconfirm pacman-mirrors"
+    REM bash -lc "pacman -S --needed --noconfirm git"
     REM Update
-    bash -lc "pacman -Syu --noconfirm"
+    REM bash -lc "pacman -Syu --noconfirm"
 
-    REM build tools
-    bash -lc "pacman -S --needed --noconfirm mingw-w64-x86_64-toolchain autoconf automake libtool make patch mingw-w64-x86_64-libtool"
+    echo "(a) setup MSYS2 for Qt build, see https://wiki.qt.io/MSYS2"
 
-    REM Set up perl
-    bash -lc "pacman -S --needed --noconfirm mingw-w64-x86_64-perl"
+    echo "-- First update msys2 core components"
+    bash -lc "pacman -Sy --noconfirm"
+    bash -lc "pacman --needed --noconfirm -S bash pacman pacman-mirrors msys2-runtime"
+
+    echo "-- Then update the rest of other components"
+    bash -lc "pacman -Su --noconfirm"
+
+    echo "-- load MinGW-w64 SEH (64bit/x86_64) posix and Dwarf-2 (32bit/i686) posix toolchains & related other tools, dependencies & components from MSYS2 REPO"
+    bash -lc "pacman -S --needed --noconfirm base-devel git mercurial cvs wget p7zip"
+    bash -lc "pacman -S --needed --noconfirm perl ruby python2 mingw-w64-i686-toolchain mingw-w64-x86_64-toolchain"
+    REM bash -lc "pacman -S --needed --noconfirm mingw-w64-x86_64-toolchain autoconf automake libtool make patch mingw-w64-x86_64-libtool"
+    REM bash -lc "pacman -S --needed --noconfirm mingw-w64-x86_64-perl"
+
     bash -lc "pl2bat $(which pl2bat)"
+
     REM bash -lc "yes | cpan App::cpanminus"
     REM bash -lc "cpanm --notest ExtUtils::MakeMaker"
 
