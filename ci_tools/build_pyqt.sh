98ae5b865a3ae445e46aa8993ebc361fb016f081
Sylvain Marie <sylvain.marie@schneider-electric.com>
2017-10-11 16:48:43 +0200

    Appveyor builds - added pyqt patch to make pyqt compile

--- ci_tools/build_pyqt.sh
+++ ci_tools/build_pyqt.sh
@@ -28,7 +28,7 @@ sudo apt-get install libglu1-mesa-dev -y
 cd $PYQT_DIR
 echo "(d) Patching PyQt configure.py in $PWD"
 # apply our HACK patch to fix the generated makefiles
-patch ./configure.py < ../ci_tools/pyqt5-$PYQT_VER-configure.py.patch
+patch ./configure.py < ../ci_tools/pyqt$PYQT_VER_SHORT-$PYQT_VER-configure.py.patch
 
 echo "(e) Configuring PyQt in $PWD"
 python configure.py --no-python-dbus --no-qml-plugin --no-qsci-api --no-tools --confirm-license --disable QtHelp --disable QtMultimedia --disable QtMultimediaWidgets --disable QtNetwork --disable QtOpenGL --disable QtPrintSupport --disable QtQml --disable QtQuick --disable QtSql --disable QtSvg --disable QtTest --disable QtWebKit --disable QtWebKitWidgets --disable QtXml --disable QtXmlPatterns --disable QtDesigner --disable QAxContainer --disable QtDBus --disable QtWebSockets --disable QtWebChannel --disable QtNfc --disable QtBluetooth --disable QtX11Extras --disable QtQuickWidgets --disable _QOpenGLFunctions_2_0 --disable _QOpenGLFunctions_2_1 --disable _QOpenGLFunctions_4_1_Core
